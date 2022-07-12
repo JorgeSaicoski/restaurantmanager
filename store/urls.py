aa78ffd7110d418d36f6b44a171a0e967eea7adb
Bardock <86751142+JorgeSaicoski@users.noreply.github.com>
2022-07-12 12:28:48 -0300

    Views and Urls update

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	#Leave as empty string for base url
+	#Change to get a specif restaurant
 	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def store(request):
+	# Change to get a specif restaurant
 	context = {}
 	return render(request, 'store/store.html', context)
 
