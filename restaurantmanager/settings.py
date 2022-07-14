0dc64dc2f578f1520b065e122b0d9022f6d23516
Bardock <86751142+JorgeSaicoski@users.noreply.github.com>
2022-07-14 16:46:54 -0300

    static fixed

--- restaurantmanager/settings.py
+++ restaurantmanager/settings.py
@@ -121,13 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/")
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/images/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
