0faa9fe22773434e8299505703d7d5cba5fc33b0
Bardock <86751142+JorgeSaicoski@users.noreply.github.com>
2022-08-17 11:18:56 -0300

    update order flow

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.models import Customer
 from store.models import Order
 # Create your views here.
@@ -6,16 +6,24 @@ def user_list(request, pk):
     # Change to get a specif restaurant
     if request.user.is_authenticated:
         user = request.user
-        customer = Customer.objects.get(user=user)
+        # if the user havent a customer
+        try:
+            customer = Customer.objects.get(user=user)
+            message = False
+        except:
+            return redirect("/account/register/customer")
+
     else:
         try:
             customer = Customer.objects.get(email=pk)
+            message = "para proteger esta pagina, registre con el mismo e-mail del pedido"
         except:
-            return render(request, 'orders/identify.html')
+            return redirect("/account/login")
     orders = Order.objects.filter(customer=customer).order_by('-date_ordered')
     context = {
         'customer':customer,
         'orders': orders,
+        'message': message,
     }
     return render(request, 'orders/list.html', context)
 
@@ -32,13 +40,15 @@ def order_detail(request, pk, email):
         return render(request, 'orders/order.html', {'order': order})
     else:
         if request.user.is_authenticated:
-            orders = Order.objects.filter(customer=customer)
+            orders = Order.objects.filter(customer=customer).order_by('-date_ordered')
+            message = "no tenes permisso a este pedido"
             context = {
                 'customer': customer,
                 'orders': orders,
+                'message': message,
             }
             return render(request, 'orders/list.html', context)
-        return render(request, 'orders/identify.html')
+        return redirect("/account/login")
 
 
 
